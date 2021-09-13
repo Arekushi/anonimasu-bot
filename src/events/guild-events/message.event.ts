6c817c404cf412a20348d5c409d3ce3a92f779dc
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2021-09-13 19:52:14 -0300

    :zap: Update Message Event

--- src/events/guild-events/message.event.ts
+++ src/events/guild-events/message.event.ts
@@ -2,29 +2,37 @@ import { Bot } from 'client/abstract-bot';
 import { Message } from 'discord.js';
 import { Event } from 'interfaces/event';
 
+let prefix = '--'
+
 export const event: Event = {
     name: 'message',
     action: async (client: Bot, message: Message) => {
-        if (isValidMessage(client, message)) {
-            const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+        prefix ??= client.config.prefix;
+
+        if (isValidMessage(message)) {
+            const args = getArgs(message.content);
             const command = client.commands.get(args.shift());
 
             if (command) {
-                command
-                    .action(client, message, args)
+                command.action(client, message, args)
                     .catch((error: any) => {
                         message.channel.send({
                             embeds: [client.embed({ description: `Error: ${error}` }, message)]
                         })
-                    })
+                })
             }
         }
     }
 }
 
-const isValidMessage = (client: Bot, message: Message): boolean => {
-    return !(message.author.bot 
-        || !message.guild 
-        || !message.content.toLocaleLowerCase().startsWith(client.config.prefix)
-    )
+const isValidMessage = (message: Message): boolean => {
+    const isValidPrefix = message.content.toLocaleLowerCase().startsWith(prefix);
+    const isBot = message.author.bot;
+    const isValidGuid = !!message.guild;
+
+    return isValidPrefix && !isBot && isValidGuid;
+}
+
+const getArgs = (value: string): string[] => {
+    return value.slice(prefix.length).trim().split(/ +/g);
 }
--- src/interfaces/command.ts
+++ src/interfaces/command.ts
@@ -4,6 +4,7 @@ import { ActionCommand } from './action';
 export interface Command {
     name: string;
     category?: Category;
+    cooldown?: number;
     description?: string;
     aliases?: string[];
     action: ActionCommand;

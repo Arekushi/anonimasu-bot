27a19270aa4a9a5576bf60da02bc3b57c3ed8dec
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2022-11-29 15:10:34 -0300

    🐛 Fix path

--- src/modules/bot/utils/file.util.ts
+++ src/modules/bot/utils/file.util.ts
@@ -3,5 +3,5 @@ import { promisify } from 'util';
 
 
 export const getFiles = async (thing: string): Promise<string[]> => {
-    return await promisify(glob)(`**/src/modules/bot/${thing}/**/*{.ts,.js}`);
+    return await promisify(glob)(`**/modules/bot/${thing}/**/*{.ts,.js}`);
 };

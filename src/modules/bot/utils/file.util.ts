c1f713300f7b9db216872f4fa495c46d3d68b1c4
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2022-11-29 15:15:46 -0300

    ⏮️ Return return to original version

--- src/modules/bot/utils/file.util.ts
+++ src/modules/bot/utils/file.util.ts
@@ -1,7 +1,9 @@
+import path from 'path';
+
 import { glob } from 'glob';
 import { promisify } from 'util';
 
 
 export const getFiles = async (thing: string): Promise<string[]> => {
-    return await promisify(glob)(`**/modules/bot/${thing}/**/*{.ts,.js}`);
+    return await promisify(glob)(`${path.dirname(require.main.filename)}/modules/bot/${thing}/**/*{.ts,.js}`);
 };
